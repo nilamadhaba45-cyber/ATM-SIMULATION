69ca2c04d731cd8e2c349d588f8b787e09785e9a
Niamadhaba Nayak <nilamadhaban993@gmail.com>
2025-09-30 18:40:14 +0530

    add menu opt 1 success

---
+++ atm.py
@@ -0,0 +1,34 @@
+print("WelCome to ATM Simulation")
+#step 1 : ATM Setup
+user_balance=10000
+atm_notes={2000:5,500:20,100:25} #note:denomination:count
+Correct_pin:1010
+
+#step 2 : ATM Verification (3 attempts pin)
+for attempt in range(3):
+    pin=int(input("Please Enter Your Pin = "))
+    if pin==Correct_pin:
+        pin_ok = True
+        break
+    else:
+        print("You Enterd Wrong Pin, Try Again.")
+if not pin_ok:                                                  #pin_ok=False, execute that
+    print("Your Card is Blocked due 3 attempts is wrong pin !")
+else:                                                           #pin_ok=true, execute that
+    print("\n ====Menu====")                                    #step 3 : menu option start choose your activity an atm
+    print("1. Check Balance.")
+    print("2. Withdrawl.")
+    print("3. Exit.")
+    choice=int(input("Enter your Choice = "))                   #ask to user What you active in atm. choose number then execute
+    if choice==1:                                               #choosing 1
+        print(f"Your Account Balance = {user_balance}")
+    if choice==2:                                               #choosing 2
+        amount=int(input("Enter your withdrawl ammount = "))    
+        if amount>user_balance:
+            print("sorry insuficient balance in your account.")
+        elif amount%100!=0:                                     
+            print("Enter amount in multiplies of 100.")
+        else:
+            withdraw_note={}                                    #withdrawl notes store
+            remaing=amount
+            
